d7761e87bfc7255a64c90f5edf6cee64c10bdfa8
Corina Talavera <corina.talavera@gmail.com>
2018-10-28 18:54:04 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -7,3 +7,13 @@ def my_find(collection)
 end
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
+end
